2e105cba5b92a10dfa3abfbd7d17bb39ab75d74b
iramirez <irving.c.ramirez.magana@intel.com>
2015-11-11 15:24:54 -0600

    tipo participacion api

---
+++ camejalActivitiesSails/api/controllers/TipoParticipacionController.js
@@ -0,0 +1,11 @@
+/**
+ * TipoParticipacionController
+ *
+ * @description :: Server-side logic for managing Tipoparticipacions
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
