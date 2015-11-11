54aa81774735e5ccd716709c74695c2fea9eadb9
iramirez <irving.c.ramirez.magana@intel.com>
2015-11-11 14:10:02 -0600

    api for cargos

---
+++ camejalActivitiesSails/api/controllers/CargosController.js
@@ -0,0 +1,11 @@
+/**
+ * CargosController
+ *
+ * @description :: Server-side logic for managing cargos
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
