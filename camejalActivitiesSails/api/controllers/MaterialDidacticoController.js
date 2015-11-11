938f581fd93c383e34ebcab21a41f26cd806026f
iramirez <irving.c.ramirez.magana@intel.com>
2015-11-11 15:22:03 -0600

    material didactico api

---
+++ camejalActivitiesSails/api/controllers/MaterialDidacticoController.js
@@ -0,0 +1,11 @@
+/**
+ * MaterialDidacticoController
+ *
+ * @description :: Server-side logic for managing Materialdidacticoes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
