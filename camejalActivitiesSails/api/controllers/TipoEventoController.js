9acea634717d6d572289ca93ead469ffedf0ce99
iramirez <irving.c.ramirez.magana@intel.com>
2015-11-11 15:24:09 -0600

    tipo de evento api

---
+++ camejalActivitiesSails/api/controllers/TipoEventoController.js
@@ -0,0 +1,11 @@
+/**
+ * TipoEventoController
+ *
+ * @description :: Server-side logic for managing Tipoeventoes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
